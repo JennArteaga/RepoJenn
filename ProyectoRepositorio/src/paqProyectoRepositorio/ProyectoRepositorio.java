33be1866ea6ff418f7addb14c24930af6015d602
windows 7 <windows 7@jenn.localdomain>
2016-05-08 16:46:31 +0200

    PrimerCommit

---
+++ ProyectoRepositorio/src/paqProyectoRepositorio/ProyectoRepositorio.java
@@ -0,0 +1,11 @@
+package paqProyectoRepositorio;
+
+public class ProyectoRepositorio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Mi proyecto de repositorio dice: Hola Mundo");
+	}
+
+}
